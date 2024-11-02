0225f8a8ec4cea51291324a041ba59d2f48a6f67
PhucPoo <hoangphuc1161998@gmail.com>
2024-11-02 09:12:38 +0700

    second commit

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,30 +1,32 @@
 import 'dart:convert';
 import 'dart:io';
+import './models/todo_model.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:frontend/views/todo_view.dart';
 import 'package:http/http.dart' as http;
+import './views/todo_view.dart';
 
-// Hàm main là điểm bắt đầu của ứng dụng
+/// Hàm main là điểm bắt đầu của ứng dụng
 void main() {
   runApp(const MainApp()); // Chạy ứng dụng với widget MainApp
 }
 
-// Widget MainApp là widget gốc của ứng dụng, sử dụng StatelessWidget
+/// Widget MainApp là widget gốc của ứng dụng, sử dụng một StatelessWidget
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Ứng dụng full-stack Flutter đơn giản',
+      debugShowCheckedModeBanner: false, // Tắt biểu tượng debug ở góc phải trên
+      title: 'Ứng dụng full-stack flutter đơn giản',
       home: TodoView(),
     );
   }
 }
 
-// Widget MyHomePage là trang chính của ứng dụng, sử dụng StatefulWidget
+/// Widget MyHomePage là trang chính của ứng dụng, sử dụng StatefulWidget
+/// để quản lý trạng thái do có nội dung cần thay đổi trên trang này
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -32,65 +34,62 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-// Lớp state cho MyHomePage
+/// Lớp state cho MyHomePage
 class _MyHomePageState extends State<MyHomePage> {
-  // Controller để lấy dữ liệu từ Widget TextField
-  final nameController = TextEditingController();
+  /// Controller để lấy dữ liệu từ Widget TextField
+  final controller = TextEditingController();
 
-  // Biến để lưu thông điệp phản hồi từ server
+  /// Biến để lưu thông điệp phản hồi từ server
   String responseMessage = '';
 
-  // Sử dụng địa chỉ IP thích hợp cho backend
-  // Do Androi Emulator sử dụng địc chỉ 10.0.2.2 để truy cập vào localhost
-  // của máy chủ thay thay vì localhost hoặc 127.0.0.1
+  /// Sử dụng địa chỉ IP thích hợp cho backend
+  /// Do Android Emulator sử dụng  địa chỉ 10.0.2.2 để truy cập vào localhost
+  /// của máy chủ thay vì localhost hoặc 127.0.0.1
   String getBackendUrl() {
     if (kIsWeb) {
-      return 'http://localhost:8080';
-      // hoặc sử dụng mạng Lan neus cần
+      return 'http://localhost:8080'; // hoặc sử dụng IP LAN nếu cần
     } else if (Platform.isAndroid) {
-      return 'http://10.0.2.2:8080';
-      // cho emulator
-      // return 'http://10.0.2.2:8080'; cho thiết bị thật khi truy cập qua LAN
+      return 'http://10.0.2.2:8080'; // cho emulator
+      // return 'http://192.168.1.x:8080'; // cho thiết bị thật khi truy cập qua LAN
     } else {
       return 'http://localhost:8080';
     }
   }
 
-  // Hàm để gửi tới server
-  Future<void> sendData() async {
-    // Lấy tên và năm sinh từ TextField
-    final name = nameController.text;
-    // Xóa nội dung trong controller
-    nameController.clear();
+  /// Hàm để gửi tên tới server
+  Future<void> sendName() async {
+    // Lấy tên từ TextField
+    final name = controller.text;
 
-    // Endpoint submit
-    final url = Uri.parse('http://localhost:8080/api/v1/submit');
+    // Sau khi lấy được tên thì xóa nội dung trong controller
+    controller.clear();
 
+    final backendUrl = getBackendUrl();
+
+    // Endpoint submit của server
+    final url = Uri.parse('$backendUrl/api/v1/submit');
     try {
       // Gửi yêu cầu POST tới server
       final response = await http
           .post(
             url,
             headers: {'Content-Type': 'application/json'},
-            body: json.encode({
-              'name': name,
-            }),
+            body: json.encode({'name': name}),
           )
           .timeout(const Duration(seconds: 10));
-
       // Kiểm tra nếu phản hồi có nội dung
-      if (response.statusCode == 200) {
+      if (response.body.isNotEmpty) {
         // Giải mã phản hồi từ server
         final data = json.decode(response.body);
-        // Cập nhật trạng thái thông điệp nhận được từ server
+
+        // Cập nhật trạng thái với thông điệp nhận được từ server
         setState(() {
-          responseMessage = data['message'] +
-              (data.containsKey('age') ? ', Tuổi: ${data['age']}' : '');
+          responseMessage = data['message'];
         });
       } else {
-        // Phản hồi không thành công
+        // Phản hồi không có nội dung
         setState(() {
-          responseMessage = 'Server trả về mã lỗi: ${response.statusCode}';
+          responseMessage = 'Không nhận được phản hồi từ server';
         });
       }
     } catch (e) {
@@ -104,39 +103,25 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Ứng dụng full-stack Flutter đơn giản',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-            fontStyle: FontStyle.italic,
-            letterSpacing: 1.5,
-          ),
-        ),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-      ),
+      appBar: AppBar(title: const Text('Ứng dụng full-stack flutter đơn giản')),
       body: Padding(
         padding: const EdgeInsets.all(18.0),
         child: Column(
           children: [
             TextField(
-              controller: nameController,
+              controller: controller,
               decoration: const InputDecoration(labelText: 'Tên'),
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: sendData,
+            FilledButton(
+              onPressed: sendName,
               child: const Text('Gửi'),
             ),
-            const SizedBox(height: 20),
             // Hiển thị thông điệp phản hồi từ server
             Text(
               responseMessage,
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
+              style: Theme.of(context).textTheme.titleLarge,
+            )
           ],
         ),
       ),
